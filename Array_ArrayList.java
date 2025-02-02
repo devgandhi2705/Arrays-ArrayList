18924afc60b5dadebfec47cae4883ce2f6c9963a
Dev gandhi <devgandhi2705@gmail.com>
2025-02-02 15:44:13 +0530

    Arrays Even and Odd

---
+++ Array_ArrayList.java
@@ -0,0 +1,7 @@
+class Array_ArrayList{
+       public static void Array_ArrayList(String args[]){
+           int[] even = new int[5];  
+           int[] odd = new int[5];             
+       }
+
+}
