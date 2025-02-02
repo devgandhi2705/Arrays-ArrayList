afdcf64cf105f0d9763760ae62e36aeb91507150
Dev gandhi <devgandhi2705@gmail.com>
2025-02-02 15:57:10 +0530

    Input

--- Array_ArrayList.java
+++ Array_ArrayList.java
@@ -1,7 +1,19 @@
+import java.util.*;
+
 class Array_ArrayList{
        public static void Array_ArrayList(String args[]){
-           int[] even = new int[5];  
-           int[] odd = new int[5];             
+           int[] even = new int[10];  
+           int[] odd = new int[10];  
+            
+           Scanner sc = new Scanner(System.in);
+           //Performing for action for 10 numbers
+           for(int i=1; i<=10;i++){
+           System.out.println("Enter a number: ");
+           int n = sc.nextInt();
+           if(n%2==0){
+              even  
+           } 
+           }
        }
 
 }
