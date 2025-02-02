9e1b20fe406a789265e14c13ba10bd81d4883095
Dev gandhi <devgandhi2705@gmail.com>
2025-02-02 16:51:58 +0530

    2nd one Started

--- Array_ArrayList.java
+++ Array_ArrayList.java
@@ -2,15 +2,17 @@ import java.util.*;
 
 class Array_ArrayList{
        public static void main(String args[]){
-           int[] even = new int[5];  
-           int[] odd = new int[5]; 
+           //1. Even and Odd Separaton into 2 different Arrays
+          
+           int[] even = new int[10];  
+           int[] odd = new int[10]; 
            int evenCount = 0; 
            int oddCount = 0;
            //int i = 0;   
             
            Scanner sc = new Scanner(System.in);
 
-           //Performing for action for 5 numbers
+           //Performing for action for 10 numbers
            for(int i=1; i<6;i++){
               System.out.println("Enter a number: ");
               int n = sc.nextInt();
@@ -29,6 +31,14 @@ class Array_ArrayList{
             System.out.print("Odd numbers: ");
             for (int k = 0; k < oddCount; k++) {
                  System.out.print(odd[k] + " ");
+            }
+          
+            //2. Implement a java function that finds 2 neighboring numbers in an array with the smallest distance to each
+
+            //For even array 
+            int temp = 0;
+            for(int n=0; n<evenCount;n++){
+                even[n]  
             }
 
        }
