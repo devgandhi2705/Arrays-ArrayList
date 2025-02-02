87a9247d3ee9ca90a310c8d325eea839fb0f1c3d
Dev gandhi <devgandhi2705@gmail.com>
2025-02-02 16:17:33 +0530

    Even & Odd

--- Array_ArrayList.java
+++ Array_ArrayList.java
@@ -1,13 +1,15 @@
 import java.util.*;
 
 class Array_ArrayList{
-       public static void Array_ArrayList(String args[]){
+       public static void main(String args[]){
            int[] even = new int[10];  
            int[] odd = new int[10]; 
            int evenCount = 0; 
-           int oddCount = 0;   
+           int oddCount = 0;
+           //int i = 0;   
             
            Scanner sc = new Scanner(System.in);
+
            //Performing for action for 10 numbers
            for(int i=1; i<=10;i++){
               System.out.println("Enter a number: ");
@@ -19,14 +21,14 @@ class Array_ArrayList{
               }
 
             System.out.print("Even numbers: ");
-            for(int i = 0; i < evenCount; i++) {
-                 System.out.print(even[i] + " ");
+            for(int j = 0; j < evenCount; j++) {
+                 System.out.print(even[j] + " ");
             }
        
         
             System.out.print("Odd numbers: ");
-            for (int i = 0; i < oddCount; i++) {
-                 System.out.print(odd[i] + " ");
+            for (int k = 0; k < oddCount; k++) {
+                 System.out.print(odd[k] + " ");
             }
 
  
